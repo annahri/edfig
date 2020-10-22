18ff5f32090bbfd7bd559f3768eb1adb879c50be
Muhammad Ahfas An Nahri <ahfas.annahri@gmail.com>
2020-10-22 16:17:15 +0700

    Refactor, change path calling mechanism

--- edfig.sh
+++ edfig.sh
@@ -7,49 +7,67 @@ set -o nounset
 config_list="$HOME/.config/configs.list"
 CMD="${0##*/}"
 
-declare -A configs
-
-get() { cut -d= -f"$1" <<< "$2" | xargs; }
-
-msg_error() { echo "$1" >&2 && exit "$2"; }
-
-usage() {
-    cat <<EOF >&2
-Usage:
-  $CMD [subcommand] [config name]
-  $CMD [config name]
-
-Example:
-  $CMD add vim "\$HOME/.vimrc"
-  $CMD vim
-  $CMD edit vim
-  $CMD rm vim
-  $CMD ls
-
-Subcommands:
-  add    Add new config file to list
-  rm     Remove config file from list
-  edit   Edit an entry
-  ls     List all stored configs
-  help   Print this
-
-EOF
+short_usage() {
+    echo -e "${bold}Usage:${reset} $CMD [subcommand|name] [...]"
     exit
 }
 
-# Load configs
-config_load() {
-    while IFS= read -r line; do
-        callname="$(get 1 "$line")"
-        configdir="$(get 2 "$line")"
-        configs[$callname]="$configdir"
-    done < <(grep '^[^\s#]\+' "$config_list")
+usage() {
+    echo -e "${bold}Usage:${reset}" >&2
+    echo -e "  $CMD [subcommand] [...]" >&2
+    echo -e "  $CMD [config name]" >&2
+    echo -e >&2
+    echo -e "${bold}Subcommands:${reset}" >&2
+    echo -e "  ${bold}add${reset}    Add new config file to list" >&2
+    echo -e "  ${bold}rm${reset}     Remove config file from list" >&2
+    echo -e "  ${bold}edit${reset}   Edit an entry" >&2
+    echo -e "  ${bold}ls${reset}     List all stored configs" >&2
+    echo -e "  ${bold}help${reset}   Print this" >&2
+    echo -e >&2
+    echo -e "${bold}Example:${reset}" >&2
+    echo -e "  $CMD add vim \"\$HOME/.vimrc\"" >&2
+    echo -e "  $CMD vim" >&2
+    echo -e "  $CMD edit vim" >&2
+    echo -e "  $CMD rm vim" >&2
+    echo -e "  $CMD ls" >&2
+    echo -e >&2
+    echo -e "$CMD is a command line tool to ease config files editing" >&2
+    exit
 }
 
-# Add config from command
+#=============================================================
+# Colors
+#=============================================================
+bold='\e[1m'
+red='\033[1;31m'
+green='\033[1;32m'
+reset='\033[0m'
+#=============================================================
+
+#=============================================================
+# Helper Functions
+#=============================================================
+msg_error() { echo -e "  ${red}x$reset $1" >&2 && exit "$2"; }
+msg_ok() { echo -e "  ${green}v$reset $1" >&2; }
+
+#=============================================================
+
+#=============================================================
+# Edfig main functions
+#=============================================================
+#
+# Get config file path from the specified config name
+#
+config_getPath() {
+    sed -n "s/^$1 *= \([^ ]*.*\)/\1/p" "$config_list"
+}
+
+#
+# Add new config path to list
+#
 config_add() {
-    test "$#" -eq 0 &&
-        msg_error "Usage: $CMD add name path" 8
+    test "$#" -ne 2 &&
+        msg_error "Usage: $CMD add <name> <config path>" 8
 
     local name="$1"
     local path="${2:-}"
@@ -60,7 +78,7 @@ config_add() {
             ;;
         *)
             if [[ "${name:0:1}" =~ ^.*([!?.,])+.*$ ]]; then
-                msg_error "Cannot use ${name:0:1} as the begining of name." 1
+                msg_error "Cannot use ${bold}${name:0:1}${reset} as the begining of name." 1
             fi
             ;;
     esac
@@ -68,20 +86,24 @@ config_add() {
     test -f "$path" ||
         msg_error "Not found: $path" 9
 
-    echo -e "$name = \"$path\"" | tee -a "$config_list" > /dev/null ||
+    echo -e "$name = $path" | tee -a "$config_list" > /dev/null ||
         msg_error "Cannot add new config." 7
 
-    echo "New config has been added."
+    msg_ok "New config has been added."
     exit
 }
 
+#
+# Modify specified config name and its path
+#
 config_edit() {
-    local name="$1"
+    local name="${1:-}"
+
     test -z "$name" &&
         $EDITOR "$config_list" && exit
 
     awk '{print $1}' "$config_list" | grep -qw "$name" ||
-        msg_error "Config for $name not found in $config_list" 1
+        msg_error "Config for ${bold}$name${reset} not found in $config_list" 1
 
     tempfile=$(mktemp /tmp/config-XXXX.tmp)
     linenum=$(grep -n "$name" "$config_list" | cut -d: -f1)
@@ -94,24 +116,27 @@ config_edit() {
 
     raw="$(head -1 "$tempfile")"
 
-    $EDITOR "$tempfile" || \
+    $EDITOR "$tempfile" ||
         msg_error "Error on $EDITOR. Aborting" 3
 
     test "$raw" == "$(head -1 "$tempfile")" &&
-        echo "No changes." >&2 && exit
+        msg_ok "No changes." && exit
 
     line="$(sed 's/"/\\"/g;s/\//\\\//g' "$tempfile")"
 
     sed "${linenum}s/.*/$line/" "$config_list" | sponge "$config_list" ||
         msg_error "Error editing entry." 4
 
-    echo "Successfully edited." >&2
+    msg_ok "Successfully edited."
     cleanup &&
         trap -- EXIT INT QUIT
 
     exit
 }
 
+#
+# Delete a config from list
+#
 config_rm() {
     local name="${1:-}"
     test "$name" ||
@@ -120,7 +145,7 @@ config_rm() {
     line=$(awk -v name="$name" '$1 == name' "$config_list")
 
     test "$line" ||
-        msg_error "Config for $name not found in $config_list" 1
+        msg_error "Config for ${bold}$name${reset} not found in $config_list" 1
 
     tempfile=$(mktemp /tmp/configs-XXXX.tmp)
 
@@ -128,13 +153,13 @@ config_rm() {
     trap cleanup EXIT INT QUIT
 
     tee "$tempfile" < "$config_list" > /dev/null ||
-        msg_error "Unable to make temporary copy." 2
+        msg_error "Unable to make temporary copy of $(basename $config_list)." 2
 
 
     grep -v "$line" "$tempfile" | tee "$config_list" > /dev/null ||
-        msg_error "Unable to remove $name from list" 4
+        msg_error "Unable to remove ${bold}$name${reset} from list" 4
 
-    echo "Successfully removed." >&2
+    msg_ok "Successfully removed."
 
     cleanup &&
         trap -- EXIT INT QUIT
@@ -142,18 +167,26 @@ config_rm() {
     exit
 }
 
+#
+# List all stored configs
+#
 config_ls() {
+    echo -e "  ${bold}Stored Configs:${reset}"
     grep '^[^\s#]\+' "$config_list" | \
-        sed 's/"//g' | \
         sort | \
-        column -s= -t
+        column -s= -t | \
+        awk '{print " ",$0}'
 
     exit
 }
 
-# Begin Script
+#=============================================================
+
+#
+# Begin Script, argument parsing
+#
 test "$#" -eq 0 &&
-    usage
+    short_usage
 
 case "$1" in
     add|rm|edit|ls)
@@ -165,42 +198,43 @@ case "$1" in
 esac
 
 test -s "$config_list" ||
-    msg_error "$config_list doesn't exist or is empty. Create it and add something first.\nExample: configname = /path/to/config" 13
+    msg_error "$config_list doesn't exist or is empty. Try adding something first.\n edfig add <name> <path>" 13
 
-config_load
-
-config_path="${configs[$name]:-}"
-
+#
+# Get config path and check if it's existed
+#
+config_path=$(config_getPath "$name")
 test "$config_path" ||
-    msg_error "$name doesn't exist." 1
+    msg_error "${bold}$name${reset} doesn't exist in configs list." 1
 
+#
+# Keeping the file extension so $EDITOR would still apply its color scheme
+#
 config_file="${config_path##*/}"
 config_ext=".${config_file##*.}"
-
 if test ."$config_file" == "$config_ext" || test "$config_file" == "$config_ext"; then
     config_tmp="$(mktemp /tmp/config-"$name"-XXXX.tmp)"
 else
     config_tmp="$(mktemp /tmp/config-"$name"-XXXX.tmp"${config_ext}")"
 fi
 
+#
+# Trap EXIT QUIT INT signal to clean temporary file
+#
 cleanup() { rm -f "$config_tmp"; }
-
 trap cleanup EXIT QUIT INT
 
-test -z "$config_path" &&
-    msg_error "Config $name is not found in $config_list" 1
-
 cp "$config_path" "$config_tmp" 2> /dev/null ||
-    msg_error "Error copying temp file. Aborting" 10
+    msg_error "Error creating temporary file. Aborting" 10
 
 $EDITOR "${config_tmp}"
 
 diff "$config_tmp" "$config_path" &> /dev/null &&
-    echo "No changes." >&2 && exit
+    msg_ok "No changes." && exit
 
 tee "$config_path" < "$config_tmp" > /dev/null ||
-    msg_error "Error ocurred." 11
+    msg_error "Cannot save changes. Aborting" 11
 
-echo "Saving changes." >&2
+msg_ok "Saving changes."
 
 cleanup && exit
